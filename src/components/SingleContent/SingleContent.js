e6eb91f7659dbe6a21c9acc5e9504d1a67d1514a
Chris Vazquez <chrisv22193@yahoo.com>
2022-03-31 17:33:04 -0700

    Fixed conditional statement for rating color

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -25,14 +25,21 @@ const classes = useStyles()
   return (
     <ContentModal media_type={media_type} id={id}>
         <Badge 
-          classes={{ badge: classes.badge }} badgeContent={vote_average} color={vote_average >= 8  ? "success" : "primary" }/>
+          classes={{ badge: classes.badge }} 
+          badgeContent={vote_average} 
+          color={
+            vote_average >= 8.8 ? "success"
+            : vote_average >= 7.5 ? "primary" 
+            : vote_average > 6.5 ? "warning" 
+            : "error"
+          }/>
         <img 
             className='poster' 
             src={ poster? `${img_300}/${poster}` : unavailable }  
             alt={title}/>
         <b className='title'> { title } </b>
         <span className='subTitle'>
-            {media_type=== "tv" ? "TV Series" : "Movie"}
+            {media_type === "tv" ? "TV Series" : "Movie"}
             <span className='subTitle'> {date} </span>
         </span>
     </ContentModal>
